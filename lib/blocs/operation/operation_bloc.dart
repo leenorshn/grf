c891dba32ed0a241bfcb1acb1bc4c25aa8f8834f
leenorshn <katembovictor@gmail.com>
2022-08-28 22:29:15 +0200

    first finish

--- lib/blocs/operation/operation_bloc.dart
+++ lib/blocs/operation/operation_bloc.dart
@@ -2,15 +2,34 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:grf/apis/operation_api.dart';
 import 'package:grf/models/operation.dart';
 
 part 'operation_event.dart';
 part 'operation_state.dart';
 
 class OperationBloc extends Bloc<OperationEvent, OperationState> {
-  OperationBloc() : super(OperationInitial()) {
-    on<OperationEvent>((event, emit) {
-      // TODO: implement event handler
+  final OperationApi operationApi;
+  StreamSubscription? subscription;
+  OperationBloc(this.operationApi) : super(OperationInitial()) {
+    on<OperationEvent>((event, emit) async {
+      if (event is LoadOpsEvent) {
+        try {
+          emit(OperationIsLoading());
+          subscription = operationApi
+              .getAll()
+              .listen((event) => add(UpdateOpsEvent(event)));
+        } on Exception {
+          emit(OperationLoadedFailure());
+        }
+      }
+
+      if (event is UpdateOpsEvent) {
+        emit(OperationLoadedSuccess(event.data));
+      }
+      if (event is AddOpsEvent) {
+        await operationApi.addBudget(event.operation);
+      }
     });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grf/apis/auth_api.dart';
+import 'package:grf/apis/budget_api.dart';
+import 'package:grf/apis/operation_api.dart';
+import 'package:grf/blocs/auth/auth_bloc.dart';
+import 'package:grf/blocs/budget/budget_bloc.dart';
+import 'package:grf/blocs/operation/operation_bloc.dart';
+import 'package:grf/login_screen.dart';
 import 'package:grf/main_screen.dart';
 
 void main() {
@@ -11,24 +19,48 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        appBarTheme: const AppBarTheme(
-          elevation: 1,
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(6),
+    return BlocProvider(
+      create: (context) => AuthBloc(AuthApi())..add(AppStartedEvent()),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+              create: (context) =>
+                  BudgetBloc(BudgetApi())..add(LoadBudgetEvent())),
+          BlocProvider(
+              create: (context) =>
+                  OperationBloc(OperationApi())..add(LoadOpsEvent())),
+        ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+            appBarTheme: const AppBarTheme(
+              elevation: 1,
+            ),
+            inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(6),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(6),
+              ),
+            ),
           ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(6),
+          home: BlocBuilder<AuthBloc, AuthState>(
+            builder: (context, state) {
+              if (state is AuthLoading) {
+                return const Scaffold(
+                    body: Center(child: CircularProgressIndicator()));
+              }
+              if (state is Authenticated) {
+                return const MainScreen();
+              }
+              return const LoginScreen();
+            },
           ),
         ),
       ),
-      home: const MainScreen(),
     );
   }
 }
